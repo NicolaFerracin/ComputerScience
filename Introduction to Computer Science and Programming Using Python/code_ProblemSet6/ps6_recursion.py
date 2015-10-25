321d16979e902e0f77d3dc1f432114f14374d495
nicola <nicola.ferracin@gmail.com>
2015-10-25 14:48:43 +0100

    pset6

---
+++ Introduction to Computer Science and Programming Using Python/code_ProblemSet6/ps6_recursion.py
@@ -0,0 +1,46 @@
+# 6.00x Problem Set 6
+#
+# Part 2 - RECURSION
+
+#
+# Problem 3: Recursive String Reversal
+#
+def reverseString(aStr):
+    """
+    Given a string, recursively returns a reversed copy of the string.
+    For example, if the string is 'abcd', the function returns 'dcba'.
+    The only string operations you are allowed to use are indexing,
+    slicing, and concatenation.
+    
+    aStr: a string
+    returns: a reversed string
+    """
+    if len(aStr) <= 0:
+        return aStr
+    return aStr[-1] + reverseString(aStr[:-1])
+
+#
+# Problem 4: X-ian
+#
+def x_ian(x, word):
+    """
+    Given a string x, returns True if all the letters in x are
+    contained in word in the same order as they appear in x.
+
+    >>> x_ian('eric', 'meritocracy')
+    True
+    >>> x_ian('eric', 'cerium')
+    False
+    >>> x_ian('john', 'mahjong')
+    False
+    
+    x: a string
+    word: a string
+    returns: True if word is x_ian, False otherwise
+    """
+    if len(x) <= 0:
+        return True
+    elif x[0] in word:
+        return x_ian(x[1:], word[word.index(x[0]):])
+    else:
+        return False
