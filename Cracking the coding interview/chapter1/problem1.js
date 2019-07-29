57b3d4108673ed5ac99f40e04484080b056c10ca
Nicola Ferracin <nicola.ferracin@gmail.com>
2019-07-29 15:23:15 -0300

    CTCI - revisit ch1/pr1

--- Cracking the coding interview/chapter1/problem1.js
+++ Cracking the coding interview/chapter1/problem1.js
@@ -2,19 +2,54 @@
 // What if you cannot use additional data structures?
 
 // Time: O(n)
-// Space: O(1)
-const hasUniqueChars = string => {
-  for (let i = 1; i < string.length; i++) {
-    if (string[i] !== string[0]) return false;
+// Space: O(n)
+// With additional data structures
+const hasUniqueChars_1 = string => {
+  const found = {};
+  for (let i = 0; i < string.length; i++) {
+    if (found[string[i]]) {
+      return false;
+    } else {
+      found[string[i]] = true;
+    }
   }
   return true;
 };
 
-console.log(hasUniqueChars('') === true);
-console.log(hasUniqueChars('a') === true);
-console.log(hasUniqueChars('  ') === true);
-console.log(hasUniqueChars('baaaaax') === false);
-console.log(hasUniqueChars('aaaaaab') === false);
-console.log(hasUniqueChars('aaabaaa') === false);
-console.log(hasUniqueChars('aaa') === true);
-console.log(hasUniqueChars('aaaa') === true);
+// Time: O(n2)
+// Space: O(1)
+// Without additional data structures
+const hasUniqueChars_2 = string => {
+  for (let i = 0; i < string.length - 1; i++) {
+    for (let j = i + 1; j < string.length; j++) {
+      if (string[i] === string[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// test cases taken from https://github.com/careercup/CtCI-6th-Edition-JavaScript-ES2015
+[
+  ['abcdefghi', true],
+  ['jklpoiuqwerzxcvmnsadf', true],
+  ['1234567890', true],
+  ['AaBbCcDdeFg1234567890(*&^%$#@!)', true],
+  ['abcadef', false],
+  ['aaaaaaaaaa', false],
+  ['abcdefghijklmnopqrstuvwxyza', false],
+  ['1234567890asdklf1', false],
+  ['!@#$%^&*()(*#($&#(*$&#*($&#()))))', false]
+].forEach(testCase => {
+  console.log('Test inputs:', testCase[0], testCase[1]);
+  console.log(
+    'With additional space:',
+    hasUniqueChars_1(testCase[0]) === testCase[1] ? 'passed' : 'failed'
+  );
+  console.log(
+    'Without additional space:',
+    hasUniqueChars_2(testCase[0]) === testCase[1] ? 'passed' : 'failed'
+  );
+  console.log('\r');
+});
